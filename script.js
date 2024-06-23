2b1c914e7156fc4036108e9fb138deda076fc53d
Arunabho Bhattacharya <barunabho@gmail.com>
2024-06-23 22:11:08 +0530

    winning combination and logic done

--- script.js
+++ script.js
@@ -21,6 +21,32 @@ function boxClicked(e) {
     if (!spaces[id]) {
         spaces[id] = currentPlayer;
         e.target.innerText = currentPlayer;
+        playerHasWon();
+        currentPlayer = currentPlayer == X_TXT ? O_TXT : X_TXT;
     }
+}
+
+//winning combination
+const winningCombination = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+
+//player win
+
+function playerHasWon(){
+    for(const condition of winningCombination){
+        let [a,b,c] = condition;
+        if(spaces[a] && spaces[a] == spaces[b] && spaces[a] == spaces[c]){
+            return [a,b,c];
+        }
+    }
+    return false;
 }
 startGame();
